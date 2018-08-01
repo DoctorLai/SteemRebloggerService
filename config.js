eec99eec0efef8b38502038510d9dc2c42983c5c
@justyy <dr.zhihua.lai@gmail.com>
2018-08-02 00:10:52 +0100

    add more nodes

--- config.js
+++ config.js
@@ -2,9 +2,19 @@
   "rpc_nodes": [
     "https://api.steemit.com",
     "https://rpc.buildteam.io",
-    "https://steemd.minnowsupportproject.org",
+    "https://rpc.steemviz.com",
+    "https://gtg.steem.house:8090",  
     "https://steemd.privex.io",
-    "https://gtg.steem.house:8090"
+    "https://seed.bitcoiner.me",
+    "https://steemd.minnowsupportproject.org",
+    "https://rpc.steemliberator.com",
+    "https://steemd.pevo.science",  
+    "https://steemd.steemitdev.com",
+    "https://steemd.steemitstage.com",
+    "https://steemd.steemgigs.org",
+    "https://appbasetest.timcliff.com",
+    "https://rpc.curiesteem.com",
+    "https://api.steem.house"
   ],
   "account": "ilovecoding",
   "posting_key": "POSTING KEY",
