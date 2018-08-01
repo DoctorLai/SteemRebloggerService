03237cd3abdf77a401ff3c29f1c0f5f5f166b097
@justyy <dr.zhihua.lai@gmail.com>
2018-08-01 22:49:59 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+{
+  "rpc_nodes": [
+    "https://api.steemit.com",
+    "https://rpc.buildteam.io",
+    "https://steemd.minnowsupportproject.org",
+    "https://steemd.privex.io",
+    "https://gtg.steem.house:8090"
+  ],
+  "account": "ilovecoding",
+  "posting_key": "POSTING KEY",
+  "tags": ["ilovecoding", "programming", "coding"],
+  "blacklist": [],
+  "interval": 3
+}
