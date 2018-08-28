c8782283735239edf09b0e2a134c7ecda14bb278
@justyy <dr.zhihua.lai@gmail.com>
2018-08-28 22:58:56 +0100

    refactoring

--- reblogger.js
+++ reblogger.js
@@ -1,37 +1,49 @@
-/*
-Usage: pm2 start reblogger.js
-Alternatively: node reblogger.js &   
-and run disown to detach
-*/
+'use strict';
 
 const fs = require("fs");
 const steem = require('steem');
-// const utils = require('utils');
 
-// read configure file
-var config = JSON.parse(fs.readFileSync("config.json"));
+const CONFIG_FILE = "config.json";
+
+var config = JSON.parse(fs.readFileSync(CONFIG_FILE));
 
 // Connect to the specified RPC node
 var rpc_node = config.rpc_nodes ? config.rpc_nodes[0] : (config.rpc_node ? config.rpc_node : 'https://api.steemit.com');
 steem.api.setOptions({ transport: 'https', uri: rpc_node, url: rpc_node });
 
-// every config.interval seconds 
-setInterval(startProcess, config.interval * 1000);
+// only allows 1 thread at a time
+var lock = false;
 
-// main entry point
-function startProcess() {
-  startReblogging(config.tags, config.blacklist);
-}
+function runInterval(func, wait, times){
+  let interv = function(w, t){
+    return function(){
+      if (typeof t === "undefined" || t-- > 0){
+        setTimeout(interv, w);
+        try {
+          func.call(null);
+        }
+        catch(e){
+          t = 0;
+          throw e.toString();
+        }
+      }
+    };
+  }(wait, times);
+  setTimeout(interv, wait);
+};
+
+// every config.interval seconds 
+runInterval(startProcess, config.interval * 1000, 99999999);
+
+startProcess();
 
 // check if y array includes any x 
 const arrayInArray = (x, y) => {
   if (!x) { return false; }
-  for (let i = 0; i < x.length; ++ i) {
-    if (y.includes(x[i])) {
-      return true;
-    }
-  }
-  return false;
+  if (!y) { return false; }
+  if (!x.constructor === Array) return false;
+  if (!y.constructor === Array) return false;
+  return x.some(r => y.includes(r));
 }
 
 // reblog
@@ -50,10 +62,9 @@ const reblog = (author1, permlink1) => {
   });  
 }
 
-// parameter: tags0 - the tags we are interested
-// parameter: blacklist - author id array
-const startReblogging = (tags0, blacklist) => {
+function startReblogging(tags0, blacklist) {
   log("Listening to Steem Blockchain...")
+  let history = new Set();
   steem.api.streamOperations((err, result) => {
     if (result && !err) {
       if (result[0] == 'comment') {
@@ -62,27 +73,31 @@ const startReblogging = (tags0, blacklist) => {
         let json_metadata = result[1]['json_metadata'];
         let parent_author = result[1]['parent_author'];
         let parent_permlink = result[1]['parent_permlink'];
-        let tags = [];
-        if (json_metadata) {
-          tags = JSON.parse(json_metadata.trim());
-          if (tags) {
-            tags = tags.tags;
+        let post = "@" + author1 + "/" + permlink1;
+        if (!history.has(post)) {
+          history.add(post);        
+          let tags = [];
+          if (json_metadata) {
+            tags = JSON.parse(json_metadata.trim());
+            if (tags) {
+              tags = tags.tags;
+            }
           }
+          if (parent_author===''&& (!permlink1.startsWith("re-")))  {
+            if (arrayInArray(tags, tags0)) {
+              if (blacklist.includes(author1)) {
+                log("blacklist author: " + author1);
+              } else {
+                log("resteem " + post);
+                reblog(author1, permlink1);   
+              }       
+            }
+          }                          
         }
-        if (parent_author===''&& (!permlink1.startsWith("re-")))  { // not a comment
-          if (arrayInArray(tags, tags0)) { // filter by tags
-            if (blacklist.includes(author1)) { // black list filter
-              log("blacklist author: " + author1);
-            } else {
-              log("resteem @" + author1 + "/" + permlink1);
-              // resteeem it
-              reblog(author1, permlink1);   
-            }       
-          }
-        }          
       }    
     } else {
       log('steem.api.streamOperations: ' + err);
+      history.clear();
       failover();
     }
   });
@@ -90,12 +105,12 @@ const startReblogging = (tags0, blacklist) => {
 
 function failover() {
   if (config.rpc_nodes && config.rpc_nodes.length > 1) {
-    var cur_node_index = config.rpc_nodes.indexOf(steem.api.options.url) + 1;
+    let cur_node_index = config.rpc_nodes.indexOf(steem.api.options.url) + 1;
 
     if (cur_node_index == config.rpc_nodes.length)
       cur_node_index = 0;
 
-    var rpc_node = config.rpc_nodes[cur_node_index];
+    let rpc_node = config.rpc_nodes[cur_node_index];
 
     steem.api.setOptions({ transport: 'https', uri: rpc_node, url: rpc_node });
     log('');
@@ -106,4 +121,21 @@ function failover() {
   }
 }
                 
-function log(msg) { console.log(new Date().toString() + ' - ' + msg); }                          
+function log(msg) { 
+  console.log(new Date().toString() + ' - ' + msg); 
+}                          
+
+function startProcess() {
+  if (lock) {
+    log('another thread already running...');
+    return;
+  }
+  lock = true;
+  try {
+    startReblogging(config.tags, config.blacklist);
+  } catch (err) {
+    log(err.message);
+  } finally {
+    lock = false;
+  }  
+}
